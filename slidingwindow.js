12851636c1545c28b1becb25d678ad69f7ea5b6c
Jadde Suarez <jaddesuarez@gmail.com>
2023-07-16 18:58:28 +0200

    Update slidingwindow.js

--- slidingwindow.js
+++ slidingwindow.js
@@ -85,3 +85,32 @@ const findBestSubarray = (nums, k) => {
     
     return ans
 }
+
+//Exercise 1: 643. Maximum Average Subarray I
+
+//https://leetcode.com/problems/maximum-average-subarray-i/
+
+//You are given an integer array nums consisting of n elements, and an integer k.
+
+//Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+const findMaxAverage = (nums, k) => {
+    let sum = 0
+
+  // Calculate the initial sum for the first 'k' elements.
+  for (let i = 0; i < k; i++) {
+    sum += nums[i]
+  }
+
+  // Set 'ans' to the initial sum.
+  let ans = sum
+
+  // Iterate through the array from index 'k' and update 'sum' and 'ans'.
+  for (let i = k; i < nums.length; i++) {
+    sum += nums[i] - nums[i - k]
+    ans = Math.max(ans, sum)
+  }
+
+  // Return the maximum average by dividing 'ans' by 'k'.
+  return ans / k
+}
